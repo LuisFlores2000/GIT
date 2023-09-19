1b0895dc890fa4ecf83729869b9d76ab3bde8cb8
LuisFlores2000 <lgflores2000@gmail.com>
2023-09-19 17:56:36 -0400

    program

--- sandbox-sandbox/Program.cs
+++ sandbox-sandbox/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Dental_Prosthetist";
+        job1._company = "Dental_Clinic_Flores";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Recepcionist";
+        job2._company = "Monterreal_Tower";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Luis Flores";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
+
