feaff15007defe87875f8ddd4db3e3d59f10beb2
LuisFlores2000 <lgflores2000@gmail.com>
2023-10-20 16:38:47 -0400

    w6

---
+++ W6/GAME.cs
@@ -0,0 +1,13 @@
+/*     Open-ended project> Hangman     <The Game
+Hangman is a classic word game in which you must guess the secret word one letter at a time.
+At the start of a game, you are presented with an set of blank lines representing the missing letters of the word.
+Each correct guess of a letter fills in the blanks.  Each incorrect guess of a letter, another part of the hangman's gallows is drawn.
+The aim of the game is to find the hidden word before the hangman's gallows is fully drawn.
+
+* Guess one letter at a time to reveal the secret word.
+* Each incorrect guess adds another part to the hangman gallows. 
+* You only get 7 incorrect guesses.
+
+
+
+
